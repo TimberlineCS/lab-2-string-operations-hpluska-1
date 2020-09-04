d13110b84a65b6a35b8d168eb1d469cedf6fc15e
Pluska <17304761+hpluska@users.noreply.github.com>
2020-09-04 10:24:13 -0600

    Add files via upload

---
+++ StringOperations.java
@@ -0,0 +1,15 @@
+public class StringOperations{
+
+  public static void main(String args[]){
+
+    //Challenge 1
+    String word ="school";
+    int wordLength = word.length();
+    //String firstHalf = word.substring(0,);
+
+    //Challenge 2
+    System.out.println("====\\====\n=====\\======");
+
+  }
+
+}
